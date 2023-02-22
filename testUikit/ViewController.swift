4c4e51df26ad8f3636b4a08a2e56bd713bbeb35c
MostafaAZ1994 <mostafa@customsurgical.co>
2023-02-22 14:39:12 +0400

    update project

--- testUikit/ViewController.swift
+++ testUikit/ViewController.swift
@@ -9,11 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func lib(_ sender: Any) {
+        print("")
+        let vc = ShowVideoEditorUIKitSwift()
+        vc.presentingViewController = self
+        vc.invokeExample()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+
+import UIKit
+import VideoEditorSDK
+import ImglyKit
+import AVFoundation
+import Foundation
+import UniformTypeIdentifiers
+
+
+
+
+class Example: NSObject {
+    /// The view controller that this example is being invoked from.
+    weak var presentingViewController: UIViewController?
+
+    func invokeExample() {
+    }
+}
+
+
+
+
+class ShowVideoEditorUIKitSwift: Example, VideoEditViewControllerDelegate {
+  override func invokeExample() {
+    // Create a `Video` from a URL to a video in the app bundle.
+    let video = Video(url: Bundle.main.url(forResource: "30 Minute Timer", withExtension: "mov")!)
+    
+
+    // Create and present the video editor. Make this class the delegate of it to handle export and cancelation.
+    let videoEditViewController = VideoEditViewController(videoAsset: video)
+    videoEditViewController.delegate = self
+    videoEditViewController.modalPresentationStyle = .fullScreen
+    presentingViewController?.present(videoEditViewController, animated: true, completion: nil)
+  }
+
+  // MARK: - VideoEditViewControllerDelegate
+
+  func videoEditViewControllerShouldStart(_ videoEditViewController: VideoEditViewController, task: VideoEditorTask) -> Bool {
+    // Implementing this method is optional. You can perform additional validation and interrupt the process by returning `false`.
+    true
+  }
+
+  func videoEditViewControllerDidFinish(_ videoEditViewController: VideoEditViewController, result: VideoEditorResult) {
+    // The user exported a new video successfully and the newly generated video is located at `result.output.url`. Dismissing the editor.
+      print(result.output.url)
+    presentingViewController?.dismiss(animated: true, completion: nil)
+  }
+
+  func videoEditViewControllerDidFail(_ videoEditViewController: VideoEditViewController, error: VideoEditorError) {
+    // There was an error generating the video.
+    print(error.localizedDescription)
+    // Dismissing the editor.
+    presentingViewController?.dismiss(animated: true, completion: nil)
+  }
+
+  func videoEditViewControllerDidCancel(_ videoEditViewController: VideoEditViewController) {
+    // The user tapped on the cancel button within the editor. Dismissing the editor.
+    presentingViewController?.dismiss(animated: true, completion: nil)
+  }
 }
 
